45e960ad4ff27b6c79de09f0946fe73f8ddece4c
David Nash <hisownspace@gmail.com>
2023-12-11 12:57:45 -0500

    add prerequest middleware

---
+++ express_app/psql-setup-script.js
@@ -0,0 +1,7 @@
+const { sequelize } = require("./db/models");
+
+sequelize.showAllSchema({ logging: false }).then(async (data) => {
+  if (!data.includes(process.env.SCHEMA)) {
+    await sequelize.createSchema(process.env.SCHEMA);
+  }
+});
